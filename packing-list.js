66eca184f3ebb3b08d5871175877f46f8cda7154
BryceHaley <bhaley91@gmail.com>
2020-09-13 16:10:58 +0000

    init packing list work

---
+++ packing-list.js
@@ -0,0 +1,16 @@
+let packingList = ["tent", "bug spray", "sleeping bag"];
+
+console.log(packingList[0]);
+console.log(packingList[1]);
+console.log(packingList[2]);
+
+////////////////////////////////////////////////////////////
+
+
+console.log('\n\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n');
+packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
