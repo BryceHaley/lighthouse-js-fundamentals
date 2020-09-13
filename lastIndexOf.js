0ce46abf8bb78f6e27ed34a5891d8d99148ed019
BryceHaley <bhaley91@gmail.com>
2020-09-13 18:45:33 +0000

    create concat, lastIndex and range functions for array manipulation practice

---
+++ lastIndexOf.js
@@ -0,0 +1,15 @@
+const lastIndexOf = function (arr, value) {
+  let lastIdx = - 1;
+  for(let idx in arr) {
+    if (arr[idx] === value) {
+      lastIdx = idx;
+    }
+  }
+  return Number(lastIdx);
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
