6b42c299449abe86860247325856a34286626739
BryceHaley <bhaley91@gmail.com>
2020-09-13 15:55:29 +0000

    write area calculator and ice-cream array manipulator

---
+++ ice-cream.js
@@ -0,0 +1,13 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+iceCreamFlavours.push('root beer');
+console.log(iceCreamFlavours);
+
+console.log(iceCreamFlavours[0]);
+
+console.log(iceCreamFlavours.slice(-1).pop());
+
+console.log(iceCreamFlavours.length);
+
+
